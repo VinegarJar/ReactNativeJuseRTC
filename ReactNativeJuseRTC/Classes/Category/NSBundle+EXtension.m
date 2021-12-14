99daa21aafd172f0e96767dd0f96a86a7e17c7d2
chenmeian <2966497308@qq.com>
2021-12-14 17:54:30 +0800

    更新资源文件路径

--- ReactNativeJuseRTC/Classes/Category/NSBundle+EXtension.m
+++ ReactNativeJuseRTC/Classes/Category/NSBundle+EXtension.m
@@ -11,22 +11,19 @@
 
 +(NSBundle*)getBundleResource{
     
-    
-    NSURL *associateBundleURL = [[NSBundle mainBundle]URLForResource:@"Frameworks"  withExtension:nil];
-    if (associateBundleURL) {
-        associateBundleURL = [[associateBundleURL URLByAppendingPathComponent:@"ReactNativeJuseRTC"] URLByAppendingPathExtension:@"framework"];
-        associateBundleURL = [[NSBundle bundleWithURL:associateBundleURL] URLForResource:@"ReactNativeJuseRTC" withExtension:@"bundle"];
-       associateBundleURL = [[NSBundle bundleWithURL:associateBundleURL] URLForResource:@"RCTResource" withExtension:@"bundle"];
+    NSURL *associateBundleURL = [[NSBundle mainBundle]URLForResource:@"ReactNativeJuseRTC"  withExtension:@"bundle"];
+    associateBundleURL = [[NSBundle bundleWithURL:associateBundleURL] URLForResource:@"RCTResource" withExtension:@"bundle"];
+    return [NSBundle bundleWithURL:associateBundleURL];
+}
 
-        if (associateBundleURL) {
-            return [NSBundle bundleWithURL:associateBundleURL];
-        }
-    }
- 
-    
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"RCTResource" ofType:@"bundle"];
-    return [NSBundle bundleWithPath:path];
-  
+
+
++(NSBundle*)getBundleResourceURL{
+    NSURL *associateBundleURL = [[NSBundle mainBundle]URLForResource:@"Frameworks"  withExtension:nil];
+    associateBundleURL = [[associateBundleURL URLByAppendingPathComponent:@"ReactNativeJuseRTC"] URLByAppendingPathExtension:@"framework"];
+    associateBundleURL = [[NSBundle bundleWithURL:associateBundleURL] URLForResource:@"ReactNativeJuseRTC" withExtension:@"bundle"];
+   associateBundleURL = [[NSBundle bundleWithURL:associateBundleURL] URLForResource:@"RCTResource" withExtension:@"bundle"];
+    return [NSBundle bundleWithURL:associateBundleURL];
 }
 
 
