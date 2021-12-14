b17181afbbcb6d041707a1cade90bae9523fc83f
chenmeian <2966497308@qq.com>
2021-12-14 16:59:42 +0800

    更新读取文件崩溃问题

--- ReactNativeJuseRTC/Classes/Category/NSBundle+EXtension.m
+++ ReactNativeJuseRTC/Classes/Category/NSBundle+EXtension.m
@@ -10,11 +10,20 @@
 @implementation NSBundle (EXtension)
 
 +(NSBundle*)getBundleResource{
+    
+    
     NSURL *associateBundleURL = [[NSBundle mainBundle]URLForResource:@"Frameworks"  withExtension:nil];
     associateBundleURL = [[associateBundleURL URLByAppendingPathComponent:@"ReactNativeJuseRTC"] URLByAppendingPathExtension:@"framework"];
     associateBundleURL = [[NSBundle bundleWithURL:associateBundleURL] URLForResource:@"ReactNativeJuseRTC" withExtension:@"bundle"];
    associateBundleURL = [[NSBundle bundleWithURL:associateBundleURL] URLForResource:@"RCTResource" withExtension:@"bundle"];
-   return [NSBundle bundleWithURL:associateBundleURL];
+
+    if (associateBundleURL) {
+        return [NSBundle bundleWithURL:associateBundleURL];
+    }
+
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"RCTResource" ofType:@"bundle"];
+    return [NSBundle bundleWithPath:path];
+  
 }
 
 
